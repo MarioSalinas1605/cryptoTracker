aa9a8ee83cb6936d09d1a308265c8987daef6cb6
Mario Salinas <mario_1605@live.com.mx>
2021-04-11 10:53:35 -0500

    feat: initial commit

---
+++ src/components/coins/CoinDetailScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class CoinDetailScreen extends Component {
+
+    render() {
+
+    }
+}
+
+export default CoinDetailScreen
