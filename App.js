b805904188d061627a3f00f548df4ecc78100e06
Mario Salinas <mario_1605@live.com.mx>
2021-04-27 07:25:53 -0500

    feat: favorites section

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Image } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import CoinsStack from './src/components/coins/CoinsStack'
+import FavoritesStack from './src/components/favorites/FavoritesStack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Colors from "./src/resources/colors";
 
@@ -31,6 +32,19 @@ const App = () => {
             )
           }}
         />
+
+        <Tabs.Screen 
+          name="Favorites"
+          component={FavoritesStack}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Image 
+                style={{ tintColor: color,  width: size, height: size }}
+                source={require('./src/assets/star.png')} 
+              />
+            )
+          }}
+        />  
       </Tabs.Navigator>
       
     </NavigationContainer>
